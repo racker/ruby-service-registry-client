58598e9d3b306c80c8ff1c871be5a1652099ec19
Jay Faulkner <jay.faulkner@rackspace.com>
2013-05-02 11:46:42 -0700

    add(set_configuration_value): Add set_configuration_value to requests

---
+++ lib/rackspace-service-registry/service-registry/requests/set_configuration_value.rb
@@ -0,0 +1,19 @@
+module Fog
+  module Rackspace
+    class ServiceRegistry
+      class Real
+
+        def set_configuration_value(configurationId, options = {})
+          data = options.dup
+          request(
+            :body     => JSON.encode(data)
+            :expects  => [204],
+            :method   => 'PUT',
+            :path     => "configuration/#{configurationId}"
+          )
+        end
+
+      end
+    end
+  end
+end
