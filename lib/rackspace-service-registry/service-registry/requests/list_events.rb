858e1051a56054e556d984740ad76464a72d3956
Jay Faulkner <jay.faulkner@rackspace.com>
2013-05-02 11:51:41 -0700

    add(list_events): Add list_events.rb to requests

---
+++ lib/rackspace-service-registry/service-registry/requests/list_events.rb
@@ -0,0 +1,18 @@
+module Fog
+  module Rackspace
+    class ServiceRegistry
+      class Real
+
+        def list_events()
+          request(
+            :expects  => [200],
+            :method   => 'GET',
+            :path     => '/events'
+          )
+        end
+
+      end
+    end
+  end
+end
+
