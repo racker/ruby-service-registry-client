e3702726f4a998016aa09105e24356e6176b9216
Dominic LoBue <dominic.lobue@gmail.com>
2013-05-02 17:53:44 -0700

    add initial implementation of send_heartbeat request

---
+++ lib/rackspace-service-registry/service-registry/requests/send_heartbeat.rb
@@ -0,0 +1,18 @@
+module Fog
+  module Rackspace
+    class ServiceRegistry
+      class Real
+
+        def send_heartbeat(serviceId, token)
+          request(
+            :body     => Fog::JSON.encode(:token => token),
+            :expects  => [200],
+            :method   => 'PUT',
+            :path     => "services/#{serviceId}/heartbeat"
+          )
+        end
+
+      end
+    end
+  end
+end
